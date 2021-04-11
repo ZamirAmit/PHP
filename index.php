1d902a8740e4e1bb12e6aa657d0f0d94a5e349ca
ZamirAmit <50704067+ZamirAmit@users.noreply.github.com>
2021-04-11 21:30:02 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
-  function writeMsg() {
-    echo "Hello world!";
-  }
+  $servername = "bgmndrlz7ef9djdqxyfg-mysql.services.clever-cloud.com:3306";
+  $username = "uuibjej9e6w3n7as";
+  $password = "mdYJyXrfpQwohZqg5jsL";
+  $db_name = "bgmndrlz7ef9djdqxyfg";
+  // Create connection
+  $conn = new mysqli($servername, $username, $password);
 
-  writeMsg(); // call the function
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  echo "Connected successfully";
 ?>
