c8de37d85c33b85733a2dbd95cb3c9db965d9b90
ZamirAmit <zamir.amit@gmail.com>
2021-04-16 13:07:51 +0300

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -16,12 +16,12 @@
         //echo "<br>";
         $sql = " SELECT `users`.`UserName` as user, " . 
         " sum(`sum`) AS amount " .
-        " FROM `TABLE 3`, " .
+        " FROM `daily_expances`, " .
         " `users` " . 
         " WHERE `date` " .
         " BETWEEN " . chr(34) . $start_Date . chr(34) .
         " AND " . chr(34) . $end_date   . chr(34) . 
-        " AND `users`.`UserID` = `TABLE 3`.`user` " . 
+        " AND `users`.`UserID` = `daily_expances`.`user` " . 
         "GROUP BY `user`";
         $users = array();
         $amount = array();
