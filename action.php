81a1a3b5ce6bdad1c06384bc879182702971c6d5
ZamirAmit <zamir.amit@gmail.com>
2021-04-16 12:52:55 +0300

    Update action.php

--- action.php
+++ action.php
@@ -20,7 +20,7 @@
     }else{
         $response["operation_error"]  = $type . " Is not recognized operation type!";
     }
-    $response["title"] = $_POST["start_date"] . "-" .  $_POST["end_date"] . " :חלוקת הוצאות תקופתית לתאריכים ";
+    $response["title"] = date($_POST["start_date"],"Y-m") . " :חלוקת הוצאות  ";
         
     $response = json_encode($response, JSON_UNESCAPED_UNICODE);
     print_r($response);
