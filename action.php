5b75edd00183b3daeda5a139e5bef6d5fddc08d6
ZamirAmit <zamir.amit@gmail.com>
2021-04-16 23:47:45 +0300

    Update action.php

--- action.php
+++ action.php
@@ -21,7 +21,7 @@ try {
     }else{
         $response["operation_error"]  = $type . " Is not recognized operation type!";
     }
-    $response["title"] = $data["title"] . " :חלוקת הוצאות  ";
+    $response["title"] = date_format($data["title"],"Y-m") . " :חלוקת הוצאות לחודש";
         
     $response = json_encode($response, JSON_UNESCAPED_UNICODE);
     print_r($response);
