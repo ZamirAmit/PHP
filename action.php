0469ac2168d6962a9fb3010d6eda5803edb4de1a
ZamirAmit <zamir.amit@gmail.com>
2021-04-17 21:13:40 +0300

    asdfadsf

--- action.php
+++ action.php
@@ -14,22 +14,26 @@ try {
             $data = get_pie_chart($start_Date,$end_date); 
             $response["chart"] = "pie-chart";
             $response["data"] = $data;
+            $response["title"] = date_format(date_create($data["title"]),"Y-m") . " :חלוקת הוצאות לחודש";
+    
+            if($chart =="bar"){
+            $data = get_bar_chart(); 
+            $response["chart"] = "bar-chart";
+            $response["data"] = $data;
+            $response["title"] = date_format(date_create($data["title"]),"Y-m") . " :סוגי הוצאות לחודש";
+    
         }else{
             $response["chart_error"] =  $char . " Is not recognized chart!";
         }
         
-    }else{
-        $response["operation_error"]  = $type . " Is not recognized operation type!";
+        }else{
+            $response["operation_error"]  = $type . " Is not recognized operation type!";
+        }
+            
+        $response = json_encode($response, JSON_UNESCAPED_UNICODE);
+        print_r($response);
     }
-    $response["title"] = date_format(date_create($data["title"]),"Y-m") . " :חלוקת הוצאות לחודש";
-        
-    $response = json_encode($response, JSON_UNESCAPED_UNICODE);
-    print_r($response);
-}
-catch (Exception $e) {
-    echo $e->getMessage();
-}
-catch (InvalidArgumentException $e) {
+}catch (Exception $e) {
     echo $e->getMessage();
 }
 
--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,5 @@
 <?php
-    function get_pie_chart($start_Date,$end_date){
+    function get_pie_chart(){
 
         $servername = "bgmndrlz7ef9djdqxyfg-mysql.services.clever-cloud.com";
         $username = "uuibjej9e6w3n7as";
@@ -67,4 +67,78 @@
         //$obj = json_encode($data, JSON_UNESCAPED_UNICODE );
         return $data;
     }
-?>
+    function get_bar_chart(){
+      
+        $servername = "bgmndrlz7ef9djdqxyfg-mysql.services.clever-cloud.com";
+        $username = "uuibjej9e6w3n7as";
+        $password = "yM1BD2Cw46IRCe89uYls";
+        $dbname = "bgmndrlz7ef9djdqxyfg";
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        $data = array();
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        $data["db_status"] =  "Connected successfully";
+        //echo "<br>";
+
+      
+      
+      
+        $sql = "SELECT `types`.`type_desc`, " . 
+               "a.amount FROM `types` " . 
+               "RIGHT JOIN ( SELECT `daily_expances`.`id_type`, " . 
+                            "SUM(`daily_expances`.`sum`) as amount " . 
+                            "FROM `daily_expances` " . 
+                            "WHERE `daily_expances`.`date` >= (SELECT `date_picker`.`DateStart` FROM `date_picker`) " . 
+                            "AND `daily_expances`.`date` <= (SELECT `date_picker`.`DateEnd` FROM `date_picker`) " . 
+                            "GROUP BY `daily_expances`.`id_type` ) " . 
+                            "a on a.`id_type` = `types`.`id_type`";
+
+        $types = array();
+        $amount = array();
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+        // output data of each row
+
+        while($row = $result->fetch_assoc()) {
+            array_push($types, $row["type_desc"] );
+            array_push($amount, $row["amount"]);
+            //echo "user: " . $row["user"]. "amount: " . $row["amount"] . "<br>";
+        }
+        } else {
+            $data["result"] =  "0 results from: "  . $sql;
+            $myfile = fopen("sql_error.txt", "w") or $data["result"] = $data["result"]  . "unable creating file_log";
+            $txt = $sql . "\n";
+            fwrite($myfile,"0 results from: " . "\n"); 
+            fwrite($myfile, $txt);
+            fclose($myfile);
+    }
+        $sql = "SELECT `date_picker`.`DateStart` FROM `date_picker`";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            // output data of each row
+    
+            while($row = $result->fetch_assoc()) {
+                $data["title"] = $row["DateStart"];
+            }
+            } else {
+                $data["result"] =  "0 results from: "  . $sql;
+                $myfile = fopen("sql_error.txt", "w") or $data["result"] = $data["result"]  . "unable creating file_log";
+                $txt = $sql . "\n";
+                fwrite($myfile,"0 results from: " . "\n"); 
+                fwrite($myfile, $txt);
+                fclose($myfile);
+        }
+        $conn->close();
+        //echo "Connection closed!";
+        //echo "<br>";
+        $data["labels"] = $types;
+        $data["values"] = $amount;
+        //$obj = json_encode($data, JSON_UNESCAPED_UNICODE );
+        return $data;
+                    
+    }
+
+    ?>
