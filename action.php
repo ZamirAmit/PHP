191a282790b6564e941f4e7069e44fcd6ab696b7
ZamirAmit <zamir.amit@gmail.com>
2021-04-19 22:26:40 +0300

    add

--- action.php
+++ action.php
@@ -1,46 +1,46 @@
 <?php
 
-try {
-    include 'dashboard.php';
+    try {
+        include 'dashboard.php';
 
-    $chart = $_POST["chart"];
-    $type = $_POST["type"];
-    $response = array();
-    // Checks request operation type:
-    if($type == "dashboard"){
-        $response["type"] = $type;
-        if($chart =="pie-chart"){
-            $data = get_pie_chart(); 
-            $response["chart"] = "pie-chart";
-            $response["data"] = $data;
-            $response["title"] = date_format(date_create($data["title"]),"Y-m") . " :חלוקת הוצאות לחודש";
-        }
-        elseif($chart =="doughnut-chart"){
-            $data = get_doughnut_chart(); 
-            $response["chart"] = "doughnut-chart";
-            $response["data"] = $data;
-            $response["title"] = date_format(date_create($data["title"]),"Y-m") . " :התפלגות הוצאות לחודש";
-       
-        }elseif($chart =="bar-per-year"){
-            $data = get_bar_chart_per_year(); 
-            $response["chart"] = "bar-per-year";
-            $response["data"] = $data;
-            $response["title"] = date_format(date_create($data["title"]),"Y") . " הוצאות בשנת";
-    
-        }else{
-            $response["chart_error"] =  $chart . " Is not recognized chart!";
-        }
+        $chart = $_POST["chart"];
+        $type = $_POST["type"];
+        $response = array();
+        // Checks request operation type:
+        if($type == "dashboard"){
+            $response["type"] = $type;
+            if($chart =="pie-chart"){
+                $data = get_pie_chart(); 
+                $response["chart"] = "pie-chart";
+                $response["data"] = $data;
+                $response["title"] = date_format(date_create($data["title"]),"Y-m") . " :חלוקת הוצאות לחודש";
+            }
+            elseif($chart =="doughnut-chart"){
+                $data = get_doughnut_chart(); 
+                $response["chart"] = "doughnut-chart";
+                $response["data"] = $data;
+                $response["title"] = date_format(date_create($data["title"]),"Y-m") . " :התפלגות הוצאות לחודש";
         
-    }else{
-            $response["operation_error"]  = $type . " Is not recognized operation type!";
-    }
+            }elseif($chart =="bar-per-year"){
+                $data = get_bar_chart_per_year(); 
+                $response["chart"] = "bar-per-year";
+                $response["data"] = $data;
+                $response["title"] = date_format(date_create($data["title"]),"Y") . " הוצאות בשנת";
+        
+            }else{
+                $response["chart_error"] =  $chart . " Is not recognized chart!";
+            }
             
-    $response = json_encode($response, JSON_UNESCAPED_UNICODE);
-    print_r($response);
+        }else{
+                $response["operation_error"]  = $type . " Is not recognized operation type!";
+        }
+                
+        $response = json_encode($response, JSON_UNESCAPED_UNICODE);
+        print_r($response);
 
-}catch (Exception $e) {
-    echo $e->getMessage();
-}
+    }catch (Exception $e) {
+        echo $e->getMessage();
+    }
 
  
 ?>
