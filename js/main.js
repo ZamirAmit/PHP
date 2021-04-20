3e9287a55d630fe2e1de044137ab3c5a51b77dd1
ZamirAmit <zamir.amit@gmail.com>
2021-04-20 21:39:24 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,31 +12,44 @@
                 var obj_pie = JSON.parse(data_pie);
 
 
-
                 //pie chart
                 var ctx = document.getElementById("pieChart");
-                document.getElementById("pieChartTitle").innerHTML = obj_pie.title;
                 if (ctx) {
                     ctx.height = 200;
                     var myChart = new Chart(ctx, {
                         type: 'pie',
-                        data: obj_pie.data.values,
-                        options: {
-                            responsive: true,
-                            plugins: {
-                                legend: {
-                                    position: 'top',
-                                },
-                                title: {
-                                    display: true,
-                                    text: 'Chart.js Pie Chart'
-                                }
-                            }
-                        },
+                        data: {
+                            datasets: [{
+                                data: pie_chart_data,
+                                backgroundColor: [
+                                    "rgba(0, 123, 255,0.9)",
+                                    "rgba(0, 123, 255,0.7)",
+                                    "rgba(0, 123, 255,0.5)",
+                                    "rgba(0,0,0,0.07)"
+                                ],
+                                hoverBackgroundColor: [
+                                    "rgba(0, 123, 255,0.9)",
+                                    "rgba(0, 123, 255,0.7)",
+                                    "rgba(0, 123, 255,0.5)",
+                                    "rgba(0,0,0,0.07)"
+                                ]
 
-                    }); ///////
+                            }],
+                            labels: pie_chart_labels
+                        },
+                        options: {
+                            legend: {
+                                position: 'top',
+                                labels: {
+                                    fontFamily: 'Poppins'
+                                }
+
+                            },
+                            responsive: true
+                        }
+                    });
                 }
-                console.log(obj_pie);
+
             });
 
     } catch (error) {
